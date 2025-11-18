6c1d2b08b4c6242fbfe7dad024ecdf19291e9cf9
Braian <braiannerd@gmail.com>
2025-11-18 17:51:38 -0300

    Automação das OPs com Expedições

--- expedicao/views.py
+++ expedicao/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from expedicao.models import Expedicao, ExpedicaoEnum
+from ordens_producao.models import OrdemProducao, StatusOPEnum
+from usuarios.models import Usuario, RoleEnum
+
+@login_required
+def listar_expedicao(request):
+    usuario = Usuario.objects.get(user=request.user)
+    if usuario.role != RoleEnum.GESTOR:
+        return render(request, 'login.html', {'mensagem': 'Acesso restrito ao gestor!'})
 
 # Create your views here.
