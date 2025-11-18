ae5bbc9fdd6aec698244f881bbcbd8445a3f9eea
Braian <braiannerd@gmail.com>
2025-11-18 09:21:15 -0300

    CRUD de Insumos

---
+++ insumos/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from insumos import views
+
+urlpatterns = [
+    path('gestor/listar_insumos', views.listar_insumos, name='listar_insumos'),
+    path('gestor/novo_insumo', views.criar_insumo, name='criar_insumo'),
+    path( 'gestor/editar_insumo/<int:pk>', views.editar_insumo, name='editar_insumo'),
+    path('gestor/excluir_insumo/<int:pk>', views.excluir_insumo, name='excluir_insumo'),
+
+]
