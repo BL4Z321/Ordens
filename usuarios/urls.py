6d52eb956b7e4527a50a72f5153de11d45e5e5c9
BL4Z321 <123798365+BL4Z321@users.noreply.github.com>
2026-04-01 12:59:32 -0300

    Adicionar dashboard para visualização de métricas e funcionalidade de exportação em PPTX

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -6,8 +6,8 @@ from usuarios import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),    
     path('sair/', views.sair, name='sair'),    
-    path('operador/dashboard', views.dashboard_operador, name='dash_operador'),
-    # path('viewer/', views.painel_viewer, name='viewer'),
+    path('operador/dashboard/', views.dashboard_operador, name='dash_operador'),
+    path('viewer/dashboard/', views.dashboard_viewer, name='viewer'),
+    path('viewer/export_pptx/', views.viewer_export_pptx, name='viewer_export_pptx'),
     path('gestor/dashboard/', views.dashboard_gestor, name='dashboard'),
-
 ]
